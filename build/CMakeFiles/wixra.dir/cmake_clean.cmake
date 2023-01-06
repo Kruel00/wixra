912e18f5004f2b63fc61aa0feb11926d312842c3
Javier Castorena <javier.castorena@reconext.com>
2023-01-05 16:13:50 -0800

    full screen mode

---
+++ build/CMakeFiles/wixra.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wixra.dir/main.c.o"
+  "CMakeFiles/wixra.dir/main.c.o.d"
+  "wixra"
+  "wixra.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/wixra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
